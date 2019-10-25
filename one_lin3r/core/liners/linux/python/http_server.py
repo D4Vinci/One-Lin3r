0e5d5a81e2e73c0fce232d3a4e83853611224264
ZonkSec <tyler@zonksec.com>
2019-10-25 09:08:08 -0500

    Update http_server.py

--- one_lin3r/core/liners/linux/python/http_server.py
+++ one_lin3r/core/liners/linux/python/http_server.py
@@ -1,5 +1,5 @@
 class info:
     author      = "ZonkSec"
-    description = "run an ad hoc http static server in your current directory"
-    function    = "dropper"
-    liner     = """python -m http.server 8000"""
+    description = "run an ad hoc http static server in your current directory (python3)"
+    function    = "Dropper"
+    liner     = """python -m http.server PORT"""
